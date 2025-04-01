eefadb397a79ecbd617d38a7e385124604982255
chaitanyarai19 <chaitanyarai19@gmail.com>
2025-04-01 10:09:57 +0530

    updated dynamic api fetching data module

--- modules/mod_apidata/helper.php
+++ modules/mod_apidata/helper.php
@@ -2,13 +2,32 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Http\HttpFactory;
+use Joomla\CMS\Log\Log;
 
 class ModApiDataHelper
 {
-    public static function getApiData()
+    public static function getApiData($params)
     {
-        $http = HttpFactory::getHttp();
-        $response = $http->get('https://jsonplaceholder.typicode.com/posts');
-        return json_decode($response->body, true);
+        $apiUrl = $params->get('api_url', 'https://jsonplaceholder.typicode.com/posts');
+
+        if (empty($apiUrl)) {
+            return [['title' => 'Error', 'body' => 'API URL is not set in module settings.']];
+        }
+
+        try {
+            $http = HttpFactory::getHttp();
+            $response = $http->get($apiUrl);
+            $data = json_decode($response->body, true);
+
+            // Ensure data is an array before returning
+            if (!is_array($data)) {
+                return [['title' => 'Error', 'body' => 'Invalid API response format.']];
+            }
+
+            return $data;
+        } catch (Exception $e) {
+            Log::add('API Fetch Error: ' . $e->getMessage(), Log::ERROR, 'mod_apidata');
+            return [['title' => 'Error', 'body' => 'Failed to fetch API data.']];
+        }
     }
 }
--- modules/mod_apidata/mod_apidata.php
+++ modules/mod_apidata/mod_apidata.php
@@ -5,5 +5,5 @@ use Joomla\CMS\Helper\ModuleHelper;
 
 require_once __DIR__ . '/helper.php';
 
-$apiData = ModApiDataHelper::getApiData();
+$apiData = ModApiDataHelper::getApiData($params); // Pass module parameters
 require ModuleHelper::getLayoutPath('mod_apidata', 'default');
